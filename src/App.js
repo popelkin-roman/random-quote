a2809b207ba79365859d704c19547314e310b31d
popelkin-roman <mymaythe23@gmail.com>
2023-12-18 20:03:15 +0500

    Styles added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div
+        id="quote-box"
+        className="position-absolute top-50 start-50 translate-middle"
+      >
+        <div className="quote-text">
+          <i className="fa fa-quote-left"> </i>
+          <span id="text">
+            The two most important days in your life are the day you are born
+            and the day you find out why.
+          </span>
+        </div>
+        <div className="quote-author">
+          - <span id="author">Mark Twain</span>
+        </div>
+        <div className="buttons">
+          <a
+            className="button"
+            id="tweet-quote"
+            title="Tweet this quote!"
+            target="_top"
+            href="/"
+          >
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a
+            className="button"
+            id="tumblr-quote"
+            title="Post this quote on tumblr!"
+            target="_blank"
+            href="/"
+          >
+            <i className="fa fa-tumblr"></i>
+          </a>
+          <button className="button btn" id="new-quote">
+            New quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
